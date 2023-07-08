df364e31880d2af4e0a1ffb970494a489aa99c5b
namanparashar123 <naman5770@gmail.com>
2023-07-08 15:31:48 -0500

    ..

--- src/Components/UI/Footer.js
+++ src/Components/UI/Footer.js
@@ -36,14 +36,14 @@ const Footer = () => {
         </div>
         <div className="col-sm-4 footerIcons">
            <a href="https://www.facebook.com/profile.php?id=100080565692833&mibextid=ZbWKwL"><img style = {{width:'35px', height: '35px', margin:'15px'}} src={fblogo} alt="logo" /></a>
-           <a href="https://www.fb.com"><img style = {{width:'35px', height: '35px', margin:'15px'}} src={whatspplogo} alt="logo" /></a>
-            <a href="https://instagram.com/navkarcourier?igshid=MzRlODBiNWFlZA=="><img style = {{width:'35px', height: '35px', margin:'15px'}} src={instalogo} alt="logo" /></a>
-            <a href=""><img style = {{width:'40px', height: '30px', margin:'15px'}} src={gmailLogo} alt="logo" /></a>
+           <a href="https://wa.me/12049993472"><img style = {{width:'35px', height: '35px', margin:'15px'}} src={whatspplogo} alt="logo" /></a>
+           <a href="https://instagram.com/navkarcourier?igshid=MzRlODBiNWFlZA=="><img style = {{width:'35px', height: '35px', margin:'15px'}} src={instalogo} alt="logo" /></a>
+           <a href="mailto:info@navkarcourier.ca"><img style = {{width:'40px', height: '30px', margin:'15px'}} src={gmailLogo} alt="logo" /></a>
         </div>
         </div>
 
         <div className="footer">
-          {`Copyright © Navkar Courier Ltd. ${year} | All Rights Reserved | Designed and developed by Naman P.`}
+          {`Copyright © Navkar Courier Ltd. ${year} | All Rights Reserved | Designed and developed by Naman P. & Sahib N.`}
         </div>
     </footer>
   );
